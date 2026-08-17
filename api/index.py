521b2dec8c880b091830f0925df6d9991f66004d
Trần Minh Thành <tranminhthanh66@gmail.com>
2026-08-17 07:55:34 +0700

    Update vercel.json configuration and remove api/index.py

--- api/index.py
+++
@@ -1,7 +0,0 @@
-import sys
-import os
-
-# Append project root to sys.path so we can import app.py correctly
-sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-from app import app
